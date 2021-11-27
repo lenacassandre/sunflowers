956eea37977e913e6728ef3e4764a9adbec496ea
dasporal <dasporal@gmail.com>
2021-11-27 18:45:43 +0100

    core yarn migration (#2)
    * chore: finished migrating
    
    * chore: finished migration
    
    * chore: fix conflict

--- example/src/views/Connexion/index.ts
+++
@@ -1,9 +0,0 @@
-import Main from "./component.main";
-import store, { StoreType } from "./script.store";
-
-import { View } from "../../../../src";
-import { User } from "../../models/User.model";
-
-const Connexion: View<User, StoreType> =  { Main, store }
-
-export default Connexion;
