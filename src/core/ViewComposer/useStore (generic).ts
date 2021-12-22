325dbd668ff2393917284e041fe0a368a4a0fee1
Harmony Bertrand <hbertrand@synexie.fr>
2021-12-22 19:51:13 +0100

    :construction: wip: started the hooks rework

--- src/core/ViewComposer/useStore (generic).ts
+++ src/core/ViewComposer/useStore (generic).ts
@@ -7,25 +7,35 @@ import Repository from "../../classes/repository.class"
 
 import log from "../../utils/log";
 import User from "../../classes/user.class";
+import { BaseRepositoriesType, BaseStoreType, Reducer } from "../..";
+
+type obj = { [storeProp: string]: any }
 
 let previousView: View<any, any> | undefined;
 
-export default function useStore<CustomUserDocument extends User>(
+export default function useStore<
+    U extends User,
+    S extends BaseStoreType = BaseStoreType,
+    R extends BaseRepositoriesType = BaseRepositoriesType,
+    A extends BaseActionsType = BaseActionsType
+>(
 	socket: Socket,
-	currentView: View<CustomUserDocument, any>,
-	session: SessionSystem<CustomUserDocument>,
-	repositoriesRef: React.MutableRefObject<Repositories>,
+	session: SessionSystem<U>,
+	router: RouterSystem<U>,
+	repositoriesRef: React.MutableRefObject<R>,
 	notify: Notify,
 	modal: CallModal,
-	router: RouterSystem<CustomUserDocument>
-): [any, Dispatch<any>, Update<{[key: string]: any}>] {
-	log.useStoreGroup();
-
-	const [storeState, setStore] = useState<{ [storeProp: string]: any }>(currentView.store);
-	const storeRef = useRef(storeState);
-	storeRef.current = storeState;
-
-	log.useStore("store", storeRef.current)
+    defaultStore: S,
+    reducer?: Reducer<U, S, R, A>
+): {
+    store: S,
+    update: Update<S>,
+    dispatch: Dispatch<A>,
+    set: (store: S) => void
+} {
+    const [store, setStore] = useState<S>(defaultStore);
+	const storeRef = useRef(store);
+    storeRef.current = store;
 
 	// La fonction upadte permet de faire des modifications immédiates au niveau du store/vue sans
 	// passer par un reducer
@@ -37,21 +47,25 @@ export default function useStore<CustomUserDocument extends User>(
 		}
 	}
 
-	// Fonction essentiel au système de vue/flux. Elle seule permet de sauvegarder le store, et donc
-	// de sauvegarder les informations affichées dans l'interface.
-	// Elle prend en argument un objet, dont les propriétés sont :
-	// - Un type d'action. (obligatoire)
-	// - Un évènement, pour que le reducer puisse accéder à la position du curseur, par exemple. (facultatif)
-	// - Des données supplémentaires. (facultatif)
-	const dispatch: Dispatch<BaseActionsType> = (type, action) => {
+	/**
+	 * Fonction essentiel au système de vue/flux. Elle seule permet de sauvegarder le store, et donc
+	 * de sauvegarder les informations affichées dans l'interface.
+	 * Elle prend en argument un objet, dont les propriétés sont :
+	 * - Un type d'action. (obligatoire)
+	 * - Un évènement, pour que le reducer puisse accéder à la position du curseur, par exemple. (facultatif)
+	 * - Des données supplémentaires. (facultatif)
+	 * @param type
+	 * @param action
+	 */
+	const dispatch: Dispatch<A> = (type, action) => {
 		// La spécification d'un type d'action est obligatoire pour déclencher une action
-		if (action && currentView && currentView.reducer) {
+		if (action && reducer) {
 			// On appelle le reducer avec toutes les informations passées en argument.
 			// Il nous renvoie un objet de mise à jour ou null.
 
 			let updates: any;
 			try {
-				updates = currentView.reducer(type, action, {
+				updates = reducer(type, action, {
 					store: storeRef.current,
 					repositories: repositoriesRef.current,
 					notify,
@@ -75,15 +89,10 @@ export default function useStore<CustomUserDocument extends User>(
 		}
 	}
 
-	useEffect(() => {
-		setStore(currentView.store)
-	}, [currentView])
-
-	log.useStoreGroupEnd("STORE");
-
-	// Réagit immédiatement au changement de vue, contrairement au useEffect
-	const storeToSend = !previousView || previousView === currentView ? storeRef.current : currentView.store;
-	previousView = currentView;
-
-	return [storeToSend, dispatch, update];
+	return {
+        store,
+        update,
+        dispatch,
+        set: setStore
+    }
 }
